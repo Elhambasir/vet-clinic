e51a93372b4fab69b9674a749af696f7e37c5e35
Elhambasir <108110651+Elhambasir@users.noreply.github.com>
2023-06-15 18:19:54 +0430

    Update data.sql
    1. Insert the following data for vets:
    Vet William Tatcher is 45 years old and graduated Apr 23rd, 2000.
    Vet Maisy Smith is 26 years old and graduated Jan 17th, 2019.
    Vet Stephanie Mendez is 64 years old and graduated May 4th, 1981.
    Vet Jack Harkness is 38 years old and graduated Jun 8th, 2008.
    
    2. Insert the following data for specialties:
    Vet William Tatcher is specialized in Pokemon.
    Vet Stephanie Mendez is specialized in Digimon and Pokemon.
    Vet Jack Harkness is specialized in Digimon.
    
    3. Insert the following data for visits:
    Agumon visited William Tatcher on May 24th, 2020.
    Agumon visited Stephanie Mendez on Jul 22th, 2020.
    Gabumon visited Jack Harkness on Feb 2nd, 2021.
    Pikachu visited Maisy Smith on Jan 5th, 2020.
    Pikachu visited Maisy Smith on Mar 8th, 2020.
    Pikachu visited Maisy Smith on May 14th, 2020.
    Devimon visited Stephanie Mendez on May 4th, 2021.
    Charmander visited Jack Harkness on Feb 24th, 2021.
    Plantmon visited Maisy Smith on Dec 21st, 2019.
    Plantmon visited William Tatcher on Aug 10th, 2020.
    Plantmon visited Maisy Smith on Apr 7th, 2021.
    Squirtle visited Stephanie Mendez on Sep 29th, 2019.
    Angemon visited Jack Harkness on Oct 3rd, 2020.
    Angemon visited Jack Harkness on Nov 4th, 2020.
    Boarmon visited Maisy Smith on Jan 24th, 2019.
    Boarmon visited Maisy Smith on May 15th, 2019.
    Boarmon visited Maisy Smith on Feb 27th, 2020.
    Boarmon visited Maisy Smith on Aug 3rd, 2020.
    Blossom visited Stephanie Mendez on May 24th, 2020.
    Blossom visited William Tatcher on Jan 11th, 2021.

--- data.sql
+++ data.sql
@@ -81,3 +81,96 @@ UPDATE animals SET owner_id = 2 WHERE name IN ('Gabumon', 'Pikachu');
 UPDATE animals SET owner_id = 3 WHERE name IN ('Devimon', 'Plantmon');
 UPDATE animals SET owner_id = 4 WHERE name IN ('Charmander', 'Squirtle', 'Blossom');
 UPDATE animals SET owner_id = 5 WHERE name IN ('Angemon', 'Boarmon');
+
+
+-- DAY 4:
+
+
+-- Insert the following data for vets:
+-- Vet William Tatcher is 45 years old and graduated Apr 23rd, 2000.
+-- Vet Maisy Smith is 26 years old and graduated Jan 17th, 2019.
+-- Vet Stephanie Mendez is 64 years old and graduated May 4th, 1981.
+-- Vet Jack Harkness is 38 years old and graduated Jun 8th, 2008.
+
+INSERT INTO vets (name, age, date_of_graduation) VALUES 
+('William Tatcher', 45, '2000-04-23'), /* 1 */
+('Maisy Smith', 26, '2019-01-17'),   /* 2 */
+('Stephanie Mendez', 64, '1981-05-04'),     /* 3 */
+('Jack Harkness', 38, '2008-06-08');    /* 4 */
+
+-- Insert the following data for specialties:
+-- Vet William Tatcher is specialized in Pokemon.
+-- Vet Stephanie Mendez is specialized in Digimon and Pokemon.
+-- Vet Jack Harkness is specialized in Digimon.
+
+INSERT INTO specializations (vet_id, species_id) VALUES 
+(1, 1), 
+(3, 2), 
+(3, 1), 
+(4, 2);
+
+/*
+  1 | Agumon     | 2020-02-03    |               0 | t        |     10.23 |          2 |        1
+  2 | Gabumon    | 2018-11-15    |               2 | t        |      8.00 |          2 |        2
+  3 | Devimon    | 2017-05-12    |               5 | t        |     11.00 |          2 |        3
+  4 | Boarmon    | 2005-06-07    |               7 | t        |     20.40 |          2 |        5
+  5 | Pikachu    | 2021-01-07    |               1 | f        |     15.04 |          1 |        2
+  6 | Blossom    | 1998-10-13    |               3 | t        |     17.00 |          1 |        4
+  7 | Plantmon   | 2021-11-15    |               2 | t        |      5.70 |          2 |        3
+  8 | Angemon    | 2005-06-12    |               1 | t        |     45.00 |          2 |        5
+  9 | Charmander | 2020-02-08    |               0 | f        |     11.00 |          1 |        4
+ 10 | Squirtle   | 1993-04-02    |               3 | f        |     12.13 |          1 |        4
+
+   id |       name       | age | date_of_graduation
+----+------------------+-----+--------------------
+  1 | William Tatcher  |  45 | 2000-04-23
+  2 | Maisy Smith      |  26 | 2019-01-17
+  3 | Stephanie Mendez |  64 | 1981-05-04
+  4 | Jack Harkness    |  38 | 2008-06-08
+(4 rows)
+*/
+
+-- Insert the following data for visits:
+-- Agumon visited William Tatcher on May 24th, 2020.
+-- Agumon visited Stephanie Mendez on Jul 22th, 2020.
+-- Gabumon visited Jack Harkness on Feb 2nd, 2021.
+-- Pikachu visited Maisy Smith on Jan 5th, 2020.
+-- Pikachu visited Maisy Smith on Mar 8th, 2020.
+-- Pikachu visited Maisy Smith on May 14th, 2020.
+-- Devimon visited Stephanie Mendez on May 4th, 2021.
+-- Charmander visited Jack Harkness on Feb 24th, 2021.
+-- Plantmon visited Maisy Smith on Dec 21st, 2019.
+-- Plantmon visited William Tatcher on Aug 10th, 2020.
+-- Plantmon visited Maisy Smith on Apr 7th, 2021.
+-- Squirtle visited Stephanie Mendez on Sep 29th, 2019.
+-- Angemon visited Jack Harkness on Oct 3rd, 2020.
+-- Angemon visited Jack Harkness on Nov 4th, 2020.
+-- Boarmon visited Maisy Smith on Jan 24th, 2019.
+-- Boarmon visited Maisy Smith on May 15th, 2019.
+-- Boarmon visited Maisy Smith on Feb 27th, 2020.
+-- Boarmon visited Maisy Smith on Aug 3rd, 2020.
+-- Blossom visited Stephanie Mendez on May 24th, 2020.
+-- Blossom visited William Tatcher on Jan 11th, 2021.
+
+
+INSERT INTO visits (animal_id, vet_id, date_of_visit) VALUES 
+(1, 1, '2020-05-24'),
+(1, 3, '2020-07-22'),
+(2, 4, '2021-02-02'),
+(5, 2, '2020-01-05'),
+(5, 2, '2020-03-08'),
+(5, 2, '2020-05-14'),
+(3, 3, '2021-05-04'),
+(9, 4, '2021-02-24'),
+(7, 1, '2019-12-21'),
+(7, 2, '2020-08-10'),
+(7, 2, '2021-04-07'),
+(10, 3, '2019-09-29'),
+(8, 4, '2020-10-03'),
+(8, 4, '2020-11-04'),
+(4, 2, '2019-01-24'),
+(4, 2, '2019-05-15'),
+(4, 2, '2020-02-27'),
+(4, 2, '2020-08-03'),
+(6, 3, '2020-05-24'),
+(6, 1, '2021-01-11');
